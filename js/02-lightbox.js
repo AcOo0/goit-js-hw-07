b9d5d1ebc6d18b66e953b3ed5c46869d398f6f21
AcOo0 <acoo.012@gmail.com>
2023-10-28 19:59:57 +0300

    Update task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+
+list.addEventListener('click', handleClick);
+list.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+let lightbox = new SimpleLightbox(".gallery__link", {
+    captionsData: "alt",
+    captionDelay: 250,    
+});
+
+function handleClick(event) { 
+    event.preventDefault();
+    if (event.target === event.currentTarget)
+        return;
+};
+
+function createMarkup(arr) { 
+    return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>
+</li>
+    `)
+        .join('');
+};
